0194e894f766eea1eace4b6cf37ddfcc195be82c
fogwww <fogin@www.com>
2017-08-27 19:40:16 +0800

    fix add

--- manage.py
+++ manage.py
@@ -2,15 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import os
-from flask_script import Manager, Shell
-from flask import request
+from flask_script import Manager, Shell, prompt_bool
+from flask import request, render_template
+from unpcmdb.extensions import db
+from unpcmdb.user.models import User
 from unpcmdb import create_app, ListConverter
 
 basepath = os.path.dirname(os.path.abspath(__file__))
 app = create_app(config=os.path.join(basepath, 'config.cfg'))
 app.url_map.converters['list'] = ListConverter
 
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+
 manager = Manager(app)
+
+
+@manager.command
+def dropdb():
+    # 清空数据
+    if prompt_bool('delete all data'):
+        db.drop_all()
+manager.add_command("sh", Shell(make_context=make_shell_context))
+
+
+@app.route('/')
+def index():
+    return render_template('hello.html')
 
 
 @app.route('/hello/<string:name>')
@@ -21,14 +40,21 @@ def hello(name):
 @app.route('/hi')
 def hi():
     # 查询字符串形式，从url中获取参数
-    s = request.args.get('user')
+    s = request.args.get('name')
     return '<h1>Hi {}!</h1>'.format(s)
 
 
-@app.route('/user/<int:uid>')
-def user(uid):
+@app.route('/name/<int:uid>')
+def u_id(uid):
     # 指定传入的uid为int类型
     return 'user id {}'.format(uid)
+
+@app.route('/t')
+def t():
+    from flask import redirect, url_for
+    print(request.endpoint)
+    return redirect(url_for('user.index'))
+
 
 
 @app.route('/home/<list:subs>')
